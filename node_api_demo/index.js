ba8407f5535d64ada5f0544bd7d5deab79d3f617
Vawnndv <dinhvanguyen810@gmail>
2022-11-10 16:55:19 +0700

    done setting

--- node_api_demo/index.js
+++ node_api_demo/index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 var bodyParser = require('body-parser')
+// const mongoose = require('mongoose')
 
+// const mongoDB = 'mongodb://localhost:27017/phone_api'
+// mongoose.connect(mongoDB, {proxyUsername: true, useUnifiedTopology: true});
+
+// const db = mongoose.connection
+// db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+
+// const phoneSchema = new mongoose.Schema({
+//     id: Number,
+//     name: String,
+//     color: String,
+//     price: String
+// })
+
+// const Phone = mongoose.model('Phone', phoneSchema)
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
@@ -11,6 +26,7 @@ const phones = [
     { id: 1, name: "IPhone X", color: "Black", price: "30000000"},
     { id: 2, name: "IPhone X PRO", color: "Green", price: "40000000"}
 ]
+
 // CRUD = Creat, Read, Update, Delete
 
 app.get("/", (req, res) => {
